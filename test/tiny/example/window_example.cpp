be40a6bf367cd26fa8dafd19b1655048fce139cc
Cédric Meuter <cedric.meuter@gmail.com>
2014-08-31 11:10:48 +0200

    INT: added first basic engine class (no time management so far)

--- test/tiny/example/window_example.cpp
+++ test/tiny/example/window_example.cpp
@@ -64,31 +64,128 @@ struct Triangle
 	std::vector<vec3> vertices;
 };
 
+
+class Engine
+{
+	using InputsCallback = std::function<void(Engine &engine, Inputs &inputs)>;
+	using UpdateCallback = std::function<void(Engine &engine, double dt)>;
+	using RenderCallback = std::function<void(Engine &engine)>;
+
+	const InputsCallback NO_INPUTS_CB = [](Engine &engine, Inputs &inputs){};
+	const UpdateCallback NO_UPDATE_CB = [](Engine &engine, double dt){};
+	const RenderCallback NO_RENDER_CB = [](Engine &engine){};
+
+public:
+	Engine(Window && window) 
+		: mWindow(std::move(window)), 
+		  mIsRunning(false), 
+		  mInputsCallback(NO_INPUTS_CB),
+		  mUpdateCallback(NO_UPDATE_CB),
+		  mRenderCallback(NO_RENDER_CB) {}
+
+	Engine(Engine &&engine) = default;
+	Engine(const Engine &other) = delete;
+	virtual ~Engine() {}
+
+	void onInputs(InputsCallback inputsCallback)
+	{
+		mInputsCallback = inputsCallback;
+	}
+
+	void onUpdate(UpdateCallback updateCallback)
+	{
+		mUpdateCallback = updateCallback;
+	}
+
+	void onRender(RenderCallback renderCallback)
+	{
+		mRenderCallback = renderCallback;
+	}
+
+	void start() 
+	{
+		if (isRunning())
+			return;
+
+ 		mIsRunning = true; 
+ 		run();
+	}
+
+	void stop() 
+	{
+		if (!isRunning())
+			return;
+
+		mIsRunning = false; 
+	}
+
+private:
+	void run()
+	{
+		while (isRunning())
+		{
+			input();
+			update();
+			render();
+		}
+	}
+	
+	bool isRunning() 
+	{
+		return mIsRunning; 
+	}
+
+	void input()
+	{
+		mInputs.refresh();
+		mInputsCallback(*this, mInputs);
+	}
+
+	void update()
+	{
+		mUpdateCallback(*this, 0.0);
+	}
+
+	void render()
+	{
+		mRenderCallback(*this);
+		mWindow.update();
+	}
+
+	Window mWindow;
+	Inputs mInputs;
+	bool mIsRunning;
+	InputsCallback mInputsCallback;
+	UpdateCallback mUpdateCallback;
+	RenderCallback mRenderCallback;
+};
+
 int main(int argc, char **argv)
 {
-	auto window = Window(1080,768, "3D");
+	auto engine = Engine(Window(1080,768, "3D"));
 	auto shaderProgram = ShaderProgram::fromFiles("res/shaders/flat_vertex.glsl", "res/shaders/flat_fragment.glsl");
 	auto bricksTexture = Texture::fromFile("res/textures/bricks.jpg");
-	auto inputs = Inputs();
 	Triangle triangle;
 
-	while (window.isOpen())
+	engine.onInputs([](Engine &engine, Inputs &inputs) 
 	{
-		inputs.refresh();
-
 		if (inputs.isWindowCloseRequested())
-			window.close();
+			engine.stop();
 
 		if (inputs.isKeyHeld(Key::KEY_LEFT_CMD) && inputs.isKeyPressed(Key::KEY_Z))
-			window.close();
+			engine.stop();
+	});
 
+	engine.onRender([&](Engine &engine) 
+	{
 		glClearColor(0,0,0.5,1);
 		glClear(GL_COLOR_BUFFER_BIT);
 
 		shaderProgram.use();
 		triangle.draw();
-		window.update();
-	}
+	});
+
+	engine.start();
 
 	return EXIT_SUCCESS;
 }
