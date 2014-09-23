3f8a3ac1fc4c249d0275b364399635940ef9dcf7
Cédric Meuter <cedric.meuter@gmail.com>
2014-09-23 22:24:53 +0200

    INT: batching light up to 16

--- test/tiny/example/testgame.cpp
+++ test/tiny/example/testgame.cpp
@@ -39,15 +39,15 @@ public:
 
 		mRenderer.setAmbientLight(vec3(1,1,1), 0.2f);
 
-		mRenderer.addDirectionalLight(vec3(1,1,1), 1.0f, vec3(1,-1,1));
-		mRenderer.addDirectionalLight(vec3(0,1,0), 0.5f, vec3(-1,-1,-1));
+		// mRenderer.addDirectionalLight(vec3(1,1,1), 1.0f, vec3(1,-1,1));
+		// mRenderer.addDirectionalLight(vec3(0,1,0), 0.5f, vec3(-1,-1,-1));
 
-		mRenderer.addPointLight(vec3(1,0,0), 0.4f, vec3(1,-1.5f,1));
-		mRenderer.addPointLight(vec3(0,1,0), 0.6f, vec3(2,-1.5f,2));
-		mRenderer.addPointLight(vec3(0,0,1), 0.6f, vec3(3,-1.5f,3));
+		// mRenderer.addPointLight(vec3(1,0,0), 0.4f, vec3(1,-1.5f,1));
+		// mRenderer.addPointLight(vec3(0,1,0), 0.6f, vec3(2,-1.5f,2));
+		// mRenderer.addPointLight(vec3(0,0,1), 0.6f, vec3(3,-1.5f,3));
 
-		mRenderer.addSpotLight(vec3(1,1,0), 1.0f, vec3(-2,-1.9,2), vec3(1,0,-1), 0.2f, 5);
-		mRenderer.addSpotLight(vec3(0,1,1), 1.0f, vec3(-3,-1.9,3), vec3(1,0,-1), 0.2f, 5);
+		// mRenderer.addSpotLight(vec3(1,1,0), 1.0f, vec3(-2,-1.9,2), vec3(1,0,-1), 0.2f, 5);
+		// mRenderer.addSpotLight(vec3(0,1,1), 1.0f, vec3(-3,-1.9,3), vec3(1,0,-1), 0.2f, 5);
 
  	 	mContext.vsync(false);			
 	}
