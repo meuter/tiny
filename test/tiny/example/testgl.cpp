478b1e0857adb8eadb35cb84090bde0bb6c8fd3a
Cédric Meuter <cedric.meuter@gmail.com>
2014-09-27 11:59:26 +0200

    INT: preparing for render to texture

--- test/tiny/example/testgl.cpp
+++ test/tiny/example/testgl.cpp
@@ -26,7 +26,7 @@ int main(int argc, char **argv)
 		.fromFiles("res/models/ground.obj", "res/models/ground.mtl")
 		.moveTo(0,-2,0);
 
-	scene.getMesh("ground").material().normalMap().fromFile("res/textures/bricks2_normal.jpg");
+	scene.getMesh("ground").material().normalMap().fromFile("res/textures/bricks_normal.jpg");
 
 	scene.addMesh("box")
 		.fromFiles("res/models/box.obj", "res/models/box.mtl")
@@ -34,7 +34,7 @@ int main(int argc, char **argv)
 		.moveTo(0,6,0);
 
 	scene.addMesh("sphere")
-		.fromFiles("res/models/sphere_flat.obj", "res/models/sphere_flat.mtl")
+		.fromFiles("res/models/sphere_smooth.obj", "res/models/sphere_flat.mtl")
 		.attachTo(scene.getMesh("ground"))
 		.moveTo(0,2,0);
 
@@ -44,7 +44,7 @@ int main(int argc, char **argv)
 		.moveTo(0,0,7)
 		.aimAt(0,0,0);
 
-	scene.addLight("d1").directional(vec3(1,1,1) * 1.5f, vec3(1,-1,1));
+	scene.addLight("d1").directional(vec3(1,1,1) * 0.8f, vec3(1,-1,1));
 	scene.addLight("d2").directional(vec3(0,1,0) * 0.5f, vec3(-1,-1,-1));
 
 	scene.addLight("p1").point(vec3(1,0,0) * 0.4f, vec3(1,-1.5f,1));
